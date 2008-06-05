9801ba14381320b888eb4ae073c99d92d88807ad
Mary P <maryp@illinois.edu>
2008-06-05 05:29:02 +0000

    Migrated components from previous library.  Added license, logger, comments.  Removed print statements.

---
+++ src/org/seasr/meandre/components/fedora/DemoFedoraClientComponent.java
@@ -0,0 +1,165 @@
+/**
+ * University of Illinois/NCSA
+ * Open Source License
+ *
+ * Copyright © 2008, NCSA.  All rights reserved.
+ * 
+ * Developed by:
+ * The Automated Learning Group
+ * University of Illinois at Urbana-Champaign
+ * http://www.seasr.org
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal with the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ * 
+ * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimers.
+ * 
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimers in
+ * the documentation and/or other materials provided with the distribution.
+ * 
+ * Neither the names of The Automated Learning Group, University of
+ * Illinois at Urbana-Champaign, nor the names of its contributors may
+ * be used to endorse or promote products derived from this Software
+ * without specific prior written permission.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS WITH THE SOFTWARE.
+ */
+
+package org.seasr.meandre.components.fedora;
+
+import java.util.logging.Logger;
+
+import org.meandre.core.ComponentContext;
+import org.meandre.core.ComponentContextException;
+import org.meandre.core.ComponentExecutionException;
+import org.meandre.core.ExecutableComponent;
+import org.meandre.core.ComponentContextProperties;
+import org.meandre.annotations.Component;
+import org.meandre.annotations.ComponentOutput;
+import fedora.server.management.FedoraAPIM;
+import fedora.server.access.FedoraAPIA;
+import fedora.client.FedoraClient;
+
+/**
+* 
+* <p>
+* Title: DemoFedoraClientComponent
+* </p>
+* 
+* <p>
+* Description: Returns a Fedora Client and APIA/APIM objects for repository
+* access and management for the demonstration Monk store.
+* </p>
+* 
+* <p>
+* Copyright: Copyright (c) 2008
+* </p>
+* 
+* <p>
+* Company: Automated Learning Group, NCSA
+* </p>
+* 
+* @author Mary Pietrowicz
+* @version 1.1
+*/
+
+@Component(
+		name="DemoFedoraClientComponent",
+		tags="fedora client APIA APIM Demo Monk",
+		creator="Mary Pierowicz",
+		description="A client for interacting with the demonstration Monk Fedora store."
+		)
+public class DemoFedoraClientComponent implements ExecutableComponent {
+
+    /* The Fedora client object output by this component. */
+	private FedoraClient fclient = null;
+	
+	/* The APIA interface object output by this component. */
+	private FedoraAPIA APIA = null;
+	
+	/* The APIM interface object output by this component. */
+	private FedoraAPIM APIM = null;
+
+	// OUTPUTS
+	@ComponentOutput(
+	description="The fedora client object.", 
+	name="fedoraClient")
+	public final static String DATA_PORT_OUT_1 = "fedoraClient";
+	
+	@ComponentOutput(
+	description="The apia interface.", 
+	name="apia")
+	public final static String DATA_PORT_OUT_2 = "apia";
+	
+	@ComponentOutput(
+	description="The apim interface.", 
+	name="apim")
+	public final static String DATA_PORT_OUT_3 = "apim";
+
+	/* The logger object to use for output. */
+	private static Logger logger = null;
+
+	public void initialize(ComponentContextProperties ccp)
+	{
+		logger = ccp.getLogger();
+		logger.info("Initializing DemoFedoraClientComponent");
+	}
+
+	public void execute(ComponentContext cc)
+	throws ComponentExecutionException, ComponentContextException
+	{
+		String fedoraURL = null;
+		String protocol = "https";
+		String host = "monk.lis.uiuc.edu";
+		String port = "443";
+		String user = "fedoraAdmin";
+		String pass = "treem0nkey";
+
+		logger.info("Firing DemoFedoraClientComponent");
+		try
+		{
+			fedoraURL = protocol+"://"+host+":"+port+"/fedora";
+			logger.info("FedoraURL: "+fedoraURL);
+
+			fclient = new FedoraClient(fedoraURL, user, pass);
+			if(fclient==null){
+				throw new ComponentExecutionException("Error getting FedoraClient");
+			}
+			APIA = fclient.getAPIA();
+			APIM = fclient.getAPIM();
+
+
+			cc.pushDataComponentToOutput(DATA_PORT_OUT_1, fclient);
+			cc.pushDataComponentToOutput(DATA_PORT_OUT_2, APIA);
+			cc.pushDataComponentToOutput(DATA_PORT_OUT_3, APIM);
+		}
+		catch (ComponentContextException ex1)
+		{
+			logger.severe("ComponentContextException error in DemoFedoraClientComponent: "+ex1.getMessage());
+			throw new ComponentContextException("Error in DemoFedoraClientComponent: "+ex1.getMessage());
+		}
+		catch (Throwable th)
+		{
+			logger.severe("ComponentExecutionException error in DemoFedoraClientComponent:  "+th.getMessage());
+			throw new ComponentExecutionException(th);
+		}
+	}
+
+	public void dispose(ComponentContextProperties ccp)
+	{
+		logger.info("Disposing DemoFedoraClientComponent");
+	}
+}
---
+++ src/org/seasr/meandre/components/fedora/GetMonkObject.java
@@ -0,0 +1,236 @@
+/**
+ * University of Illinois/NCSA
+ * Open Source License
+ *
+ * Copyright © 2008, NCSA.  All rights reserved.
+ * 
+ * Developed by:
+ * The Automated Learning Group
+ * University of Illinois at Urbana-Champaign
+ * http://www.seasr.org
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal with the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ * 
+ * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimers.
+ * 
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimers in
+ * the documentation and/or other materials provided with the distribution.
+ * 
+ * Neither the names of The Automated Learning Group, University of
+ * Illinois at Urbana-Champaign, nor the names of its contributors may
+ * be used to endorse or promote products derived from this Software
+ * without specific prior written permission.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS WITH THE SOFTWARE.
+ *//**
+ * University of Illinois/NCSA
+ * Open Source License
+ *
+ * Copyright © 2008, NCSA.  All rights reserved.
+ * 
+ * Developed by:
+ * The Automated Learning Group
+ * University of Illinois at Urbana-Champaign
+ * http://www.seasr.org
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal with the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ * 
+ * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimers.
+ * 
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimers in
+ * the documentation and/or other materials provided with the distribution.
+ * 
+ * Neither the names of The Automated Learning Group, University of
+ * Illinois at Urbana-Champaign, nor the names of its contributors may
+ * be used to endorse or promote products derived from this Software
+ * without specific prior written permission.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS WITH THE SOFTWARE.
+ */
+
+package org.seasr.meandre.components.fedora;
+
+import java.util.logging.Logger;
+
+import org.meandre.core.ComponentContext;
+import org.meandre.core.ComponentContextException;
+import org.meandre.core.ComponentExecutionException;
+import org.meandre.core.ExecutableComponent;
+import org.meandre.core.ComponentContextProperties;
+import org.meandre.annotations.Component;
+import org.meandre.annotations.ComponentInput;
+import org.meandre.annotations.ComponentOutput;
+import org.meandre.annotations.ComponentProperty;
+import fedora.server.access.FedoraAPIA;
+import fedora.server.types.gen.MIMETypedStream;
+import fedora.server.types.gen.Property;
+
+/**
+ * 
+ * <p>
+ * Title: GetMonkOject
+ * </p>
+ * 
+ * <p>
+ * Description: Gets an html fragment for a requested resource in a demonstration Monk Fedora store.
+ * </p>
+ * 
+ * <p>
+ * Copyright: Copyright (c) 2008
+ * </p>
+ * 
+ * <p>
+ * Company: Automated Learning Group, NCSA
+ * </p>
+ * 
+ * @author Mary Pietrowicz
+ * @version 1.1
+ */
+
+@Component(
+		creator="Mary Pietrowicz", 
+		description="Gets an html fragment for an object from the Monk fedora store.",
+		tags="fedora XML Monk Demo", 
+		name="GetMonkObject")
+public class GetMonkObject implements ExecutableComponent
+{
+	//INPUT
+	@ComponentInput(
+	description="An APIA connection", 
+	name=FedoraConstants.APIA)
+	final String DATA_INPUT_1 = FedoraConstants.APIA;
+
+	//PROPERTY
+	@ComponentProperty(
+	description = "The fedora ID of the object to get",
+	name = FedoraConstants.PID, 
+	defaultValue = "monk:ncf-22511")
+	final static String PROPERTY1 = FedoraConstants.PID;
+	
+	@ComponentProperty(
+	description = "The behavior definition ID",
+	name = FedoraConstants.B_DEF_PID, 
+	defaultValue = "monk:behav-def-book")
+	final static String PROPERTY2 = FedoraConstants.B_DEF_PID;
+	
+	@ComponentProperty(
+	description = "The method name to call",
+	name=FedoraConstants.METHOD_NAME, 
+	defaultValue = "getChunk")
+	final static String PROPERTY3 = FedoraConstants.METHOD_NAME;
+	
+	@ComponentProperty(
+	description = "The workpart ID",
+	name = "WorkpartID", 
+	defaultValue = "ncf-22511-1")
+	final static String PROPERTY4 = "WorkpartID";
+
+	//OUTPUT
+	@ComponentOutput(
+	description="The xml object", 
+    name="HTMLFrag")
+	final String DATA_OUTPUT_1="HTMLFrag";
+	
+	FedoraAPIA APIA = null;
+
+	/* The logger object to use for output. */
+	private static Logger logger = null;
+	
+	public void initialize(ComponentContextProperties ccp)
+	{	
+		logger = ccp.getLogger();
+		logger.info("Initializing GetMonkObject");
+	}
+	
+	/*
+	 * Gets an html fragment for a requested component in the Monk Fedora
+	 * store.
+	 * 
+	 * Inputs: an APIA connection to fedora
+	 * 
+	 * Properties:
+	 * pid:  The fedora id of the object to get
+	 * bDefPid: The behavior definition id
+	 * methodName: The method name to call
+	 * 
+	 * Outputs:
+	 * HTMLFrag: the html fragment of a Monk object representation
+	 * @see org.meandre.core.ExecutableComponent#execute(org.meandre.core.ComponentContext)
+	 */
+	public void execute(ComponentContext cc)
+	  throws ComponentExecutionException, ComponentContextException
+	  {
+		 logger.info("Firing GetMonkObject");
+		 try
+		 {
+			logger.info("Getting one XML object...");
+			String pid =  cc.getProperty(PROPERTY1);
+			String b_def_pid = cc.getProperty(PROPERTY2);
+			String method_name = cc.getProperty(PROPERTY3);
+			String workpartId = cc.getProperty(PROPERTY4);
+
+			APIA = (FedoraAPIA) cc.getDataComponentFromInput(DATA_INPUT_1);
+
+			Property propertiesArray[] = new Property[1];
+			propertiesArray[0] = new Property();
+			propertiesArray[0].setName("xmlid");
+			propertiesArray[0].setValue(workpartId);
+			System.out.println("Initialized property array.");
+			
+			MIMETypedStream mt_stream = null;
+			
+			//String output_html_fragment = "tester output";
+			
+			mt_stream = APIA.getDissemination(pid, b_def_pid, method_name, 
+					propertiesArray, null);
+		    String output_html_fragment = new String(mt_stream.getStream());
+			
+		    logger.info(output_html_fragment);
+		    cc.pushDataComponentToOutput(DATA_OUTPUT_1, output_html_fragment);
+		 }
+		 catch (ComponentContextException ex1)
+		 {
+				logger.severe("Error in GetMonkObject: "+ex1.getMessage());
+				throw new ComponentContextException("Error in GetMonkObject: "+ex1.getMessage());
+		  }
+		  catch (Throwable th)
+		  {
+				logger.severe("Error in GetMonkObject:  "+th.getMessage());
+				throw new ComponentExecutionException(th);
+		  }		 
+	  }
+
+	public void dispose(ComponentContextProperties ccp)
+    {
+       logger.info("Disposing GetMonkObject");
+	}
+
+}
